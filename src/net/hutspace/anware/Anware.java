30a3ed8e2b6615eed77d8ff427084e156051ac2f
Emmanuel Okyere <chief@hutspace.net>
2011-04-27 09:46:37 +0000

    First commit. :)

---
+++ src/net/hutspace/anware/Anware.java
@@ -0,0 +1,13 @@
+package net.hutspace.anware;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Anware extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
