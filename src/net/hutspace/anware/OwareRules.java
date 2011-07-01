2ce40d84f2eab399fa7c3b3821a67352d103ef88
Emmanuel Okyere <chief@hutspace.net>
2011-07-01 22:39:55 +0000

    Added missed files from last commit and eclipse project files

---
+++ src/net/hutspace/anware/OwareRules.java
@@ -0,0 +1,13 @@
+package net.hutspace.anware;
+
+import net.hutspace.anware.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OwareRules extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game_rules);
+	}
+}
